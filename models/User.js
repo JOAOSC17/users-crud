313ecd9647c78886c14c6987b7ae60c489b933da
João Costa <costav1402@gmail.com>
2022-04-01 21:14:54 -0300

    find all controller

--- models/User.js
+++ models/User.js
@@ -14,4 +14,4 @@ const UserSchema = new mongoose.Schema({
         required:[true, 'Please add a age']
     }
 })
-module.exports = mongoose.model.User || mongoose.model('User', UserSchema)
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema)
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,19 +1,28 @@
+import User from "../../models/User";
 import dbConnect from "../../utils/dbConnect"
-import User from "../../models/User"
 dbConnect()
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   const { method } = req;
   switch (method) {
+    case 'GET':
+      try {
+        const users = await User.find({})
+        res.status(201).json(users)
+      } catch (error) {
+        res.status(400).json("Erro")
+      }
+      break;
     case 'POST':
       try {
         const user = await User.create(req.body)
-        res.status(201).json({sucess:true, data:user})
+        res.status(201).json(user)
       } catch (error) {
-        res.status(400).json({sucess:false})
+        res.status(400).json("Erro")
       }
       break;
     default:
-      res.status(400).json({sucess:false})
+      res.status(400).json("Erro")
       break;
   }
 }
